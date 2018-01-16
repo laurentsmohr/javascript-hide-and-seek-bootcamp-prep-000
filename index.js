fb78002151a12c201040fa14ede690b8f1c93c0b
Laurents Mohr <laurents.mohr@whu.edu>
2018-01-16 23:57:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var children = document.querySelector('#grand-node')
+  return children[children.length - 1];
+}
